f0e44397dac1f4c8d01d1817c3e271051606d5b8
Bitnair <Bitnair@users.noreply.github.com>
2021-12-13 21:30:38 -0600

    class added

--- Boredless_Tourist.py
+++ Boredless_Tourist.py
@@ -1,15 +1,12 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
-traveler_destination_index = location_object.get_destination_index(location_object.get_traveler_location.traveler_destination)
+traveler_destination_index = ''
 
-class Location:
-    def get_destination_index(destination):
-        destination_index = destinations.index(destination)
-        return destination_index
 
-    def get_traveler_location(traveler):
-        # traveler_destination = destinations[get_destination_index.destination_index]
-        traveler_destination = test_traveler[1]
-        return traveler_destination_index
+def get_destination_index(destination):
+    destination_index = destinations.index(destination)
+    return destination_index
 
-location_object = Location()
+def get_traveler_location(traveler):
+    traveler_destination = test_traveler[1]
+    return traveler_destination_index
